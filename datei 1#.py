df98d8fff15db563ae09767fcf67c68ada4f6f27
LM <43296951+lm-development@users.noreply.github.com>
2019-05-30 12:16:00 +0200

    Add files via upload

---
+++ datei 1#.py
@@ -0,0 +1,29 @@
+a = 2
+print(a)
+
+array = [1,2,3,4,5,6,7,8,9]
+for element in array: #einfache for schleife 
+    print (element)
+
+
+
+print("continue program")
+
+
+
+
+
+print("Enter your name")
+x = input()
+print(x)
+
+
+
+
+
+
+
+
+
+
+
---
+++ datei 3#.py
@@ -0,0 +1,20 @@
+import os
+
+
+while True:
+    
+    z = input()
+    n = len(z)
+    print(n)
+
+    
+    #if z == "exit":
+        
+       #break
+    #print("Programm beendet")
+
+    
+
+       
+        
+        
